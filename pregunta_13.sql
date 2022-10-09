c40beff10c6b807e802a58d4ad30f5c648e3df8a
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-10-09 06:17:07 -0500

    Update pregunta_13.sql

--- pregunta_13.sql
+++ pregunta_13.sql
@@ -42,4 +42,13 @@
 --  4  E  474.825000
 -- 
 --  >>> Escriba su codigo a partir de este punto <<<
---  
+--  SELECT
+     K0, avg(c12)
+FROM
+    tbl1
+WHERE
+	c13>400
+GROUP BY
+	k0
+ORDER BY
+	K0 ASC;
